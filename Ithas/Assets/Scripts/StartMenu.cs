fa6e127af9089b7d8070409efe1c3e3b6af0e7a7
Celine LIM (celine.l) <limsl.celine@gmail.com>
2023-07-04 15:44:20 +0800

    added menu and new map!

--- Ithas/Assets/Scripts/StartMenu.cs
+++ Ithas/Assets/Scripts/StartMenu.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class StartMenu : MonoBehaviour
+namespace Ithas
 {
-    // Start is called before the first frame update
-    void Start()
+    public class StartMenu : MonoBehaviour
     {
-        
-    }
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
     }
 }
+
